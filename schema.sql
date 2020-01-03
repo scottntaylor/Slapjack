1a91ce496265e373376b8f030e80d587c37a615d
Scott Taylor <scottntaylor21@gmail.com>
2020-01-02 18:30:11 -0800

    Schema Update

--- schema.sql
+++ schema.sql
@@ -1,4 +1,17 @@
 -- Drops the blogger if it exists currently --
-DROP DATABASE IF EXISTS cardDeck_db;
+DROP DATABASE IF EXISTS slapjack_db;
 -- Creates the "blogger" database --
-CREATE DATABASE cardDeck_db;
+CREATE DATABASE slapjack_db;
+
+USE DATABASE slapjack_db;
+
+CREATE TABLE card_Deck(
+    Name VARCHAR(30) NOT NULL,
+    Value Integer(2) NOT NULL
+
+);
+
+CREATE TABLE Users(
+    User_name VARCHAR(30) NOT NULL,
+    Score Integer(10) NOT Null
+);
